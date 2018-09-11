6bb726d954cbccebcf9df4912e4a122c3a4477cd
kvatoferma <jimmaggot@mail.ru>
2018-09-11 17:15:18 +0300

    Add personFactory( ) and setLovers() function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,46 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const personFactory = function (name, loves) {
+    return {
+      name: name,
+      loves: loves,
+      lovedBy: []
+    }
+  }
+  const people = [];
+  preferences.forEach((element, index) => {
+    people.push(personFactory(index+1, element));
+  });
+
+  (function setLovers() {
+    people.forEach((person) => {
+      people[person.loves -1].lovedBy.push(person.name);
+    });
+  })();
+
+  function countLoveTriangles() {
+
+  }
+
+
+  console.log(people);
+
 };
+getLoveTrianglesCount([2, 3, 1, 5, 6, 4]);
+
+  //   if (this.includesIndex(element)) {
+  //
+  //   }
+  // });
+
+  // Array.prototype.includesIndex = function(index) {
+  //   return (index < this.length) || false;
+  // }
+  //   let count;
+  // preferences.forEach((element, index) => {
+  //   if (preferences.[preferences[preferences[index]]] === preferences[index]) {
+  //     count++;
+  //   }
+  // });
+  // return count;
