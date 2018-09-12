0841d74fa91957b81bf0290fa4bdda2f5abd3a06
kvatoferma <jimmaggot@mail.ru>
2018-09-13 00:06:44 +0300

    Add some logic

--- src/index.js
+++ src/index.js
@@ -2,29 +2,57 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-// module.exports = 
-function getLoveTrianglesCount(preferences = []) {
+// module.exports =
+// function getLoveTrianglesCount(preferences = [2,3,1]) {
+(function getLoveTrianglesCount(people = [2,3,1]) {
   const personFactory = function (name, loves) {
     return {
       name: name,
       loves: loves,
-      lovedBy: []
     }
   }
-  const people = [];
-  preferences.forEach((element, index) => {
-    people.push(personFactory(index+1, element));
-  });
 
-  (function setLovers() {
-    people.forEach((person) => {
-      people[person.loves -1].lovedBy.push(person.name);
-    });
-  })();
+people = [2,3,1]
 
-  function countLoveTriangles() {
+name: index
+loves: people[index]
 
-  }
+//*****************************
+// [null, 2, 3, 1]
+// 2) Устраняем смещение, чтобы отсчет элементов шёл с i=1:
+    let triangles = [];
+    people.unshift(null);
+// 3) Получаем ссылку на любимого:
+    // people.forEach((element, i) => {
+      for (var i = 1; i <= array.length; i++) {
+        let love = people[i]; // => 2
+        let loveOfLove = people[love]; // => 3
+// 5) Сравниваем значения:
+        if (people[loveOfLove] === i) {
+          triangle = [
+            personFactory(i, people[i]),
+            personFactory(love, people[love]),
+            personFactory(loveOfLove, people[loveOfLove])
+          ];
+        }
+// 8) Если данного треугольника нету в массиве, кладем его туда:
+
+// Сравнение объектов:
+// JSON.stringify(obj1) === JSON.stringify(obj2);
+
+
+        if (!triangles.includes(triangle)) { triangles.push(triangle) }
+
+
+// 9) Повторяем для каждого элемента people[i] пока не переберём весь массив people:
+//      people.forEach((element, i) => { ... });
+// 10) Вычисляем длину массива triangles, это и есть кол-во любовных треугольников:
+//      return triangles.length
+    }
+    // });
+
+//***************************
+
   // Алгоритм:
   // 1) Имеется массив:
   //      people = [2, 3, 1];
@@ -32,10 +60,10 @@ function getLoveTrianglesCount(preferences = []) {
   //      people.unshift(null); => [null, 2, 3, 1];
   // 3) Получаем ссылку на любимого:
   //      love = people[i]; => 2
-  // 4) Переходим по ссылке:
+  // 4) Получаем ссылку на любомого любимого:
   //      loveOfLove = people[love]; => 3
   // 5) Сравниваем значения:
-  //      if (people[loveOfLove] === people[i])
+  //      if (people[loveOfLove] === i)
   // 6) Если true, создаем треугольник в виде массива объектов:
   //      triangle = [
   //        personFactory(i, people[i]),
@@ -45,12 +73,13 @@ function getLoveTrianglesCount(preferences = []) {
   // 7) Создаем массив для хранения треугольников:
   //      triangles = []
   // 8) Если данного треугольника нету в массиве, кладем его туда:
-  //      if (!triangles.includes(triangle)) { triangles.push(triangle) }
+  //      for (var i = 0; i < triangles.length; i++) {
+  //        if (!JSON.stringify(triangles[i]) === JSON.stringify(triangle)) { triangles.push(triangle) }
+  //      }
   // 9) Повторяем для каждого элемента people[i] пока не переберём весь массив people:
   //      people.forEach((element, i) => { ... });
   // 10) Вычисляем длину массива triangles, это и есть кол-во любовных треугольников:
   //      return triangles.length
-
 
 
 
@@ -67,8 +96,7 @@ function getLoveTrianglesCount(preferences = []) {
 
   console.log(people);
 
-};
-getLoveTrianglesCount([2, 3, 1, 5, 6, 4]);
+})();
 
   //   if (this.includesIndex(element)) {
   //
